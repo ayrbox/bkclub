7001830544e48f752b7ea4538c9457ee6178aed3
Sabin Raj Dangol <sawbean.raz@googlemail.com>
2019-02-13 14:38:24 +0000

    check changes

--- EtherStore.sol
+++ EtherStore.sol
@@ -10,9 +10,7 @@ contract EtherStore {
 
     function withdrawFunds (uint256 _weiToWithdraw) public {
         require(balances[msg.sender] >= _weiToWithdraw);
-        // limit the withdrawal
         require(_weiToWithdraw <= withdrawalLimit);
-        // limit the time allowed to withdraw
         require(now >= lastWithdrawTime[msg.sender] + 1 weeks);
         require(msg.sender.call.value(_weiToWithdraw)());
         balances[msg.sender] -= _weiToWithdraw;
