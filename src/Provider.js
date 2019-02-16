3d2d832d665dacbebc5f9f52bb297d08c10f80a7
Sabin Raj Dangol <sawbean.raz@googlemail.com>
2019-02-16 00:53:56 +0000

    Add theme header with bookclub

--- src/Provider.js
+++ src/Provider.js
@@ -4,20 +4,37 @@ import Logo from './component/logo'
 
 const headerStyle = {
   height: '80px',
-  padding: '10px 6.25vw',
+  padding: '15px 6.25vw',
   position: 'absolute',
   top:0,
   left: 0,
   right: 0,
   background: '#fff',
   boxShadow: 'rgba(22, 22, 86, 0.15) 0px 0px 20px 0px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  verticalAlign: 'top'
+}
+
+const containerStyle = {
+  marginTop: '80px'
+}
+
+const bookClubStyle = {
+  fontSize: '28px',
+  weight: '900'
 }
 
 const Provider = ({ children, ...rest }) => (
   <ThemeProvider {...rest}>
-    {children}
+    <div style={containerStyle}>
+      {children}
+    </div>
     <div style={headerStyle} >
       <Logo />
+      <div style={bookClubStyle}>
+        #bookclub
+      </div>
     </div>
 
   </ThemeProvider>
